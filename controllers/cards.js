5b4ba8ea5d8c72241d4535cbd3e194ba35910423
Evgenia Suslina <zhenechka-95@yandex.ru>
2022-09-23 00:30:29 +0300

    14 sprint 3

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,7 +1,7 @@
-const Card = require("../models/card");
+const Card = require('../models/card');
 
-const BadRequestError = require("../utils/errors/bad_request");
-const NotFoundError = require("../utils/errors/not_found");
+const BadRequestError = require('../utils/errors/bad_request');
+const NotFoundError = require('../utils/errors/not_found');
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
@@ -16,8 +16,8 @@ module.exports.createCard = (req, res, next) => {
   Card.create({ name, link, owner })
     .then((card) => res.send(card))
     .catch((err) => {
-      if (err.name === "ValidationError") {
-        next(new BadRequestError("Переданы некорректные данные"));
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные'));
       } else {
         next(err);
       }
@@ -29,14 +29,14 @@ module.exports.deleteCard = (req, res, next) => {
 
   Card.findByIdAndRemove(cardId)
     .orFail(() => {
-      throw new Error("NotFound");
+      throw new Error('NotFound');
     })
     .then((cards) => res.send(cards))
     .catch((err) => {
-      if (err.name === "CastError") {
-        next(new BadRequestError("Невалидный id"));
-      } else if (err.message === "NotFound") {
-        next(new NotFoundError("Карточка с указанным _id не найдена."));
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Невалидный id'));
+      } else if (err.message === 'NotFound') {
+        next(new NotFoundError('Карточка с указанным _id не найдена.'));
       } else {
         next(err);
       }
@@ -50,17 +50,17 @@ module.exports.addlikeToCard = (req, res, next) => {
   Card.findByIdAndUpdate(
     cardId,
     { $addToSet: { likes: userId } },
-    { new: true }
+    { new: true },
   )
     .orFail(() => {
-      throw new Error("NotFound");
+      throw new Error('NotFound');
     })
     .then((cards) => res.send(cards))
     .catch((err) => {
-      if (err.name === "CastError") {
-        next(new BadRequestError("Передан некорректный id"));
-      } else if (err.message === "NotFound") {
-        next(new NotFoundError("Передан несуществующий _id карточки"));
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Передан некорректный id'));
+      } else if (err.message === 'NotFound') {
+        next(new NotFoundError('Передан несуществующий _id карточки'));
       } else {
         next(err);
       }
@@ -73,14 +73,14 @@ module.exports.deleteLikeFromCard = (req, res, next) => {
 
   Card.findByIdAndUpdate(cardId, { $pull: { likes: userId } }, { new: true })
     .orFail(() => {
-      throw new Error("NotFound");
+      throw new Error('NotFound');
     })
     .then((cards) => res.send(cards))
     .catch((err) => {
-      if (err.name === "CastError") {
-        next(new BadRequestError("Передан некорректный id"));
-      } else if (err.message === "NotFound") {
-        next(new NotFoundError("Передан несуществующий _id карточки"));
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Передан некорректный id'));
+      } else if (err.message === 'NotFound') {
+        next(new NotFoundError('Передан несуществующий _id карточки'));
       } else {
         next(err);
       }
--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,12 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/user");
-const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
 
-const BadRequestError = require("../utils/errors/bad_request");
-const UnauthorizedError = require("../utils/errors/unauthorized");
-const NotFoundError = require("../utils/errors/not_found");
-const ConflictError = require("../utils/errors/conflict");
+
+const BadRequestError = require('../utils/errors/bad_request');
+const UnauthorizedError = require('../utils/errors/unauthorized');
+const NotFoundError = require('../utils/errors/not_found');
+const ConflictError = require('../utils/errors/conflict');
 
 module.exports.getUsers = (req, res, next) => {
   User.find({})
@@ -18,14 +19,14 @@ module.exports.getUserById = (req, res, next) => {
 
   User.findById(userId)
     .orFail(() => {
-      next(new NotFoundError("Пользователь не найден"));
+      next(new NotFoundError('Пользователь не найден'));
     })
     .then((users) => res.send(users))
     .catch((err) => {
-      if (err.name === "CastError") {
-        next(new BadRequestError("Введен невалидный id"));
-      } else if (err.message === "NotFound") {
-        next(new NotFoundError("Пользователь по указанному _id не найден."));
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Введен невалидный id'));
+      } else if (err.message === 'NotFound') {
+        next(new NotFoundError('Пользователь по указанному _id не найден.'));
       } else {
         next(err);
       }
@@ -33,10 +34,12 @@ module.exports.getUserById = (req, res, next) => {
 };
 
 module.exports.createUser = (req, res, next) => {
+
   const { name, about, avatar, email, password } = req.body;
   bcrypt
     .hash(password, 10)
     .then((hash) =>
+
       User.create({
         name,
         about,
@@ -56,9 +59,9 @@ module.exports.createUser = (req, res, next) => {
     )
     .catch((err) => {
       if (err.code === 11000) {
-        next(new ConflictError("Почта уже существует"));
-      } else if (err.name === "ValidationError") {
-        next(new BadRequestError("Переданы некорректные данные"));
+        next(new ConflictError('Почта уже существует'));
+      } else if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные'));
       } else {
         next(err);
       }
@@ -75,14 +78,14 @@ module.exports.updateProfile = (req, res, next) => {
     { new: true, runValidators: true, upsert: false }
   )
     .orFail(() => {
-      throw new Error("NotFound");
+      throw new Error('NotFound');
     })
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === "ValidationError") {
-        next(new BadRequestError("Переданы некорректные данные"));
-      } else if (err.message === "NotFound") {
-        next(new NotFoundError("Пользователь по указанному _id не найден."));
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные'));
+      } else if (err.message === 'NotFound') {
+        next(new NotFoundError('Пользователь по указанному _id не найден.'));
       } else {
         next(err);
       }
@@ -99,14 +102,14 @@ module.exports.updateAvatar = (req, res, next) => {
     { new: true, runValidators: true, upsert: false }
   )
     .orFail(() => {
-      throw new Error("NotFound");
+      throw new Error('NotFound');
     })
     .then((user) => res.status(200).send(user))
     .catch((err) => {
-      if (err.name === "ValidationError") {
-        next(new BadRequestError("Переданы некорректные данные"));
-      } else if (err.message === "NotFound") {
-        next(new NotFoundError("Пользователь по указанному _id не найден."));
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные'));
+      } else if (err.message === 'NotFound') {
+        next(new NotFoundError('Пользователь по указанному _id не найден.'));
       } else {
         next(err);
       }
